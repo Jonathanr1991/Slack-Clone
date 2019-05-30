ef14de2189047318d1eda15ea28ac168269d3d34
jrodriquez <jrodri30@students.towson.edu>
2019-05-29 22:38:27 -0400

    project setup

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jonathanrodriquez/Documents/SlackClone/backend/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
